1f70af90563f94f735f70716377718a4c61bf789
lovdaniel11 <lovdaniel11@gmail.com>
2024-09-04 20:26:01 -0700

    beginning reactflow integration

--- client/src/Pages/Main.js
+++ client/src/Pages/Main.js
@@ -2,20 +2,69 @@
 import FileInput from "../Components/FileInput";
 
 // Package Imports
-import { Auth0Provider } from '@auth0/auth0-react';
+import React, { useCallback, useState } from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
+import {
+    ReactFlow,
+    Controls,
+    Background,
+    useNodesState,
+    useEdgesState,
+    addEdge,
+  } from '@xyflow/react';
+   
+  import '@xyflow/react/dist/style.css';
+   
 
 
 const Main = () => {
 
+
+    const initialNodes = [
+        {
+          id: 'CSE_11',
+          type: 'default',
+          position: { x: 0, y: 0 },
+          data: { label: 'CSE 11' }
+        },
+        {
+            id: 'CSE_12',
+            type: 'default',
+            position: { x: 0, y: 0 },
+            data: { label: 'CSE 12' }
+          }
+      ];
+
+      const initialEdges = [];
+
     const LoadContents = () => {
         const { user, isAuthenticated, loginWithRedirect, logout } = useAuth0();
+        const [nodes, , onNodesChange] = useNodesState(initialNodes);
+        const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+      
+        const onConnect = useCallback(
+          (params) => setEdges((eds) => addEdge(params, eds)),
+          [setEdges],
+        );
+        
         
         if (isAuthenticated) {
             let user_content = user.sub.split("|");
             let userID = user_content[1];
-            return (<div>
+
+            return (<div style={{width: "100vw",height: '100vw' }}>
                 <FileInput></FileInput>
+                <ReactFlow
+                nodes={nodes}
+                edges={edges}
+                onNodesChange={onNodesChange}
+                onEdgesChange={onEdgesChange}
+                onConnect={onConnect}
+                fitView
+            >
+                <Background/>
+                <Controls/>
+                </ReactFlow>
                 <p> user id: {userID}</p>
                 </div>)
         }
